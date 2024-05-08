4b79aacc26ef4f5ed3543d7dc5dffeed9e4d48ed
daviardev <jerson.silva99@hotmail.com>
2024-05-07 22:44:23 -0500

    Create main layout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,55 @@
+import Link from 'next/link'
+
+import { RiHome5Fill } from 'react-icons/ri'
+import { HiOutlineUsers } from 'react-icons/hi2'
+import { TbMessage2 } from 'react-icons/tb'
+import { FaRegUser } from 'react-icons/fa'
+
+import { Upload } from './SvgConverted'
+
+export default function Footer () {
+  return (
+    <>
+      <footer className='w-full justify-evenly text-sm text-black p-2 gap-5 bottom-0 border-t-[1px] border-[rgba(22,24,35,.2)]'>
+        <div className='flex justify-between items-center'>
+          <Link
+            href='/'
+            className='flex flex-col items-center'
+          >
+            <RiHome5Fill size={22} />
+            <span>Home</span>
+          </Link>
+          <Link
+            href='/users'
+            className='flex flex-col items-center'
+          >
+            <HiOutlineUsers size={22} />
+            <span>Friends</span>
+          </Link>
+          <Link
+            href='/upload'
+            className='flex flex-col items-center'
+          >
+            <div className='w-[fit-content] flex items-center justify-center my-0 mx-auto top-2/4 left-2/4'>
+              <Upload />
+            </div>
+          </Link>
+          <Link
+            href='/inbox'
+            className='flex flex-col items-center'
+          >
+            <TbMessage2 size={22} />
+            <span>Inbox</span>
+          </Link>
+          <Link
+            href='/profile[id]'
+            className='flex flex-col items-center'
+          >
+            <FaRegUser size={22} />
+            <span>Profile</span>
+          </Link>
+        </div>
+      </footer>
+    </>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Header () {
+  return (
+    <>
+      <header className='flex justify-start pl-4'>
+        <Link href='/'>
+          <Image
+            src='/images/tiktok-logo.png'
+            alt='TikTok Logo'
+            width={20}
+            height={20}
+            className='w-auto h-auto'
+            priority
+          />
+        </Link>
+      </header>
+    </>
+  )
+}
