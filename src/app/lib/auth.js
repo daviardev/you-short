aea8248a916faf678d27182895d6d2404614e325
daviardev <jerson.silva99@hotmail.com>
2024-05-11 16:44:09 -0500

    Fix error auth, change name env variable -_-

--- src/app/lib/auth.js
+++ src/app/lib/auth.js
@@ -1,7 +1,7 @@
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.SDK_CLIENT_ID || '',
