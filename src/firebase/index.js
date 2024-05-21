f020d994ac2f9276cf1db4c58321ca7cfbd2f264
daviardev <jerson.silva99@hotmail.com>
2024-05-21 13:57:14 -0500

    Rename env variables and import firestore, storage

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,12 +1,21 @@
 import { initializeApp, getApp, getApps } from 'firebase/app'
 
+import { getStorage } from 'firebase/storage'
+import { getFirestore } from 'firebase/firestore'
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_ENV_APIKEY,
-  authDomain: process.env.NEXT_ENV_AUTHDOMAIN,
-  projectId: process.env.NEXT_ENV_PROJECTID,
-  storageBucket: process.env.NEXT_ENV_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_ENV_MESSAGINGSENDERID,
-  appId: process.env.NEXT_ENV_APPID
+  apiKey: process.env.NEXT_PUBLIC_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_APPID
+
 }
 
-export const app = !getApps.length() ? initializeApp(firebaseConfig) : getApp()
+export const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+
+const db = getFirestore()
+const storage = getStorage()
+
+export { db, storage }
