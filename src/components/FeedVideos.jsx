149dac5be0fb52464ef7f400843dd5e239688485
daviardev <jerson.silva99@hotmail.com>
2024-05-21 14:47:38 -0500

    Render videos

--- src/components/FeedVideos.jsx
+++ src/components/FeedVideos.jsx
@@ -1,52 +1,34 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+
+import { db } from '@/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+
 import VideoPlayer from './VideoPlayer'
 
-const VIDEOS = [
-  {
-    id: 1,
-    author: 'penelope',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod #food #omg #divertido #fun #epic #hashtag',
-    likes: 123,
-    comments: 321,
-    shares: 12,
-    songName: 'Problemón - Alvaro Diaz & Rauw Alejandro',
-    avatar: '/images/image.jpeg',
-    src: '/Download.mp4',
-    albumCover: '/images/cover.jpg'
-  },
-  {
-    id: 2,
-    author: 'lavacalola',
-    description: 'lorem ipsum dolorectetur adipiscing, tempor incididunt ut labore et dolore magna aliqua. #food #omg #divertido #fun #epic #hashtag',
-    likes: 321,
-    comments: 123,
-    shares: 23,
-    songName: 'sonido original - pablitocastilloo',
-    avatar: '/images/image.jpeg',
-    src: '/Download.mp4',
-    albumCover: '/images/cover.jpg'
-  },
-  {
-    id: 3,
-    author: 'fumanchu',
-    description: 'tempor incididunt ut labore et dolore magna aliqua. #food #omg #divertido #fun #epic #hashtag',
-    likes: 321,
-    comments: 123,
-    shares: 23,
-    songName: 'sonido original - A L E X 🥀',
-    avatar: '/images/image.jpeg',
-    src: '/Download.mp4',
-    albumCover: '/images/cover.jpg'
-
-  }
-]
+const generateUniqueKey = (video) => `${video.id}-${video.src}`
 
 export default function FeedVideos () {
-  return VIDEOS.map(video => (
-    <div
-      key={video.id}
-      className='w-full h-full snap-center'
-    >
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const querySnapshot = await getDocs(collection(db, 'videos'))
+      const videoData = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }))
+
+      setVideos(videoData)
+    }
+
+    fetchVideos()
+  }, [])
+
+  return (videos.map(video => (
+    <div key={generateUniqueKey(video)} className='w-full h-full snap-center'>
       <VideoPlayer {...video} />
     </div>
-  ))
+  )))
 }
