7d2ed211781c59792220e97372a4ad47a43f62be
daviardev <jerson.silva99@hotmail.com>
2024-07-20 12:58:10 -0500

    testing route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,29 +1,5 @@
 import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
-
-export const authOptions = {
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    GoogleProvider({
-      clientId: process.env.SDK_CLIENT_ID || '',
-      clientSecret: process.env.SDK_SECRET_CLIENT || ''
-    })
-  ],
-  callbacks: {
-    async session ({ session, token }) {
-      session.user.tag = session.user.name
-        .split(' ')
-        .join('')
-        .toLocaleLowerCase()
-
-      session.user.uid = token.sub
-      return session
-    }
-  },
-  pages: {
-    signIn: '/signin'
-  }
-}
+import { authOptions } from '@/app/lib/auth'
 
 export const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
