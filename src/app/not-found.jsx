7223c54e78e63241924e89c7a934293bcc01e21f
daviardev <jerson.silva99@hotmail.com>
2024-07-20 12:58:01 -0500

    Change tags

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -2,19 +2,17 @@ import Button from '@/components/Utils/Button'
 
 import { TbError404 } from 'react-icons/tb'
 
-export default function NotFoundPage () {
+export default function NotFound () {
   return (
-    <main className='container flex flex-col mx-auto pt-12 my-24'>
-      <div className='text-center'>
-        <span className='text-red-600 flex justify-center'>
-          <TbError404 size={100} />
-        </span>
-        <h2 className='text-3xl font-bold tracking-tight sm:text-5xl'>Page not found</h2>
-        <p className='mt-6 text-base leading-7'>Sorry, we couldn’t find the page you’re looking for.</p>
-        <div className='flex items-center justify-center gap-x-6'>
-          <Button href='/'>Go back home</Button>
-        </div>
+    <section className='container flex flex-col mx-auto pt-12 my-24 text-center'>
+      <span className='text-red-600 flex justify-center'>
+        <TbError404 size={100} />
+      </span>
+      <h2 className='text-3xl font-bold tracking-tight sm:text-5xl'>Page not found</h2>
+      <p className='mt-6 text-base leading-7'>Sorry, we couldn’t find the page you’re looking for.</p>
+      <div className='flex items-center justify-center gap-x-6'>
+        <Button href='/'>Go back home</Button>
       </div>
-    </main>
+    </section>
   )
 }
